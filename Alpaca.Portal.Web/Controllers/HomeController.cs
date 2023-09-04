606e84ed6c977bd7a99e75325d58c21b0f9253f6
su-sh <su-sh@MSI>
2023-09-04 21:19:03 +0900

    プロジェクト ファイルを追加します。

---
+++ Alpaca.Portal.Web/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Alpaca.Portal.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Alpaca.Portal.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Index");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            _logger.LogInformation("Privacy");
+            return View();
+        }
+
+        [ActionName("User")]
+        public IActionResult UserPage()
+        {
+            _logger.LogInformation("User");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            _logger.LogInformation("Error");
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
